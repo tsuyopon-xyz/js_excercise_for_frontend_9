2e2722111276e81976397c2bd2c61be8513b9ac8
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2018-12-21 13:23:24 +0900

    Createad an assignment for frontend part 9.

---
+++ README.md
@@ -0,0 +1,36 @@
+# レポジトリ内容
+
+このレポジトリは[Web白熱教室](https://tsuyopon.xyz/)の[JavaScriptの学習コンテンツ > JavaScriptフロントエンド編](https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/)にある「[【エクササイズ】async/awaitを使ってクイズアプリを作る](https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/js-excercise-for-frontend-9/)」で利用するものです。
+
+### 完成形(表示内容はリロードする度に変わる)
+
+![完成形](./images/assignment.gif)
+
+
+## 課題
+
+以下の「課題をクリアするためのステップ」の内容を全て対応して、`index.html`をブラウザで開き、上の完成形の画像と同じような機能を実装する。(API(ネットワーク)経由でのデータの取得が完了されるまではクイズデータの表示はされないことに注意する。)
+
+基本的には「[【エクササイズ】Fetch APIを使ってクイズアプリを作る](https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/js-excercise-for-frontend-8/)」と同じアプリの実装になる。
+
+異なる点は「async/await」を使った実装になったところ。そのため、レビュー時は「async/await」を正しく使えているかがポイントとなる。
+
+
+### 課題をクリアするためのステップ
+
+1. [ ] main.jsの中のコメントに記述されている課題をクリアしてください。
+
+## 答え
+
+- 答えは `answer.html`, `answer.js`, `answer.css` の中身になります。参考記事を読んでもわからなくなった場合は、答えの実装を見て、コードの流れを理解してからまたチャレンジしてみてください。(コピペ厳禁)
+  - `answer.html`, `answer.js`, `answer.css` はあくまでも解答の一例のため、参考する実装例の1つとして認識する。
+
+## 参考記事
+
+課題をこなしていて、行き詰まったら以下の資料を参考にしていただければ解答にたどり着けるかと思います。
+
+
+- Web白熱教室
+  - [JavaScriptのasyncとawaitの概要【Promiseの発展系】](https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/how-to-use-the-fetch-api-in-js/)
+  - [【JavaScript】asyncとawaitの使い方【サンプルコード付き】](https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/how-to-use-async-and-await-in-js/)
+  - [【JavaScript】async/awaitでの例外処理の方法](https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/how-to-handle-exception-in-async-await/)
---
+++ answer.js
@@ -0,0 +1,222 @@
+// 解答例 (色々な実装方法があるため、この実装はあくまでも1つの実装例として考えること。)
+
+(() => {
+  // 利用するAPI情報元
+  //   - 本サイト : https://opentdb.com/
+  //   - 利用するAPI : https://opentdb.com/api.php?amount=10&type=multiple
+
+  const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
+
+  // 「gameState」オブジェクトを作る
+  // - クイズアプリのデータ管理用オブジェクト
+  // - 保持する情報
+  //   - quizzes : fetchで取得したクイズデータの配列(resutls)を保持する
+  //   - currentIndex : 現在何問目のクイズに取り組んでいるのかをインデックス番号で保持する
+  //   - numberOfCorrects : 正答数を保持するう
+  const gameState = {
+    quizzes : [],
+    currentIndex : 0,
+    numberOfCorrects : 0
+  };
+
+  // HTMLのid値がセットされているDOMを取得する
+  const questionElement = document.getElementById('question');
+  const resultElement = document.getElementById('result');
+  const answersContainer = document.getElementById('answers');
+  const restartButton = document.getElementById('restart-button');
+
+  // ページの読み込みが完了したらクイズ情報を取得する
+  window.addEventListener('load', (event) => {
+    fetchQuizData();
+  });
+
+  // 「Restart」ボタンをクリックしたら再度クイズデータを取得する
+  restartButton.addEventListener('click', (event) => {
+    fetchQuizData();
+  });
+
+
+  // `fetchQuizData関数`を実装する
+  // - 実現したいこと
+  //   - async/awaitを使って実装する
+  //   - Webページ上の表示をリセットする
+  //     - id属性値が 'question' の要素に「Now loading...」という文字列をセットする
+  //     - id属性値が 'result' の要素に空文字列をセットする (Restartボタンでクイズ2回以上繰り返す時用)
+  //     - id属性値が 'restart-button' の要素を非表示にする (Restartボタンでクイズ2回以上繰り返す時用)
+  //   - クイズ取得~取得後の流れ
+  //     1. API_URLとFetch API(fetchメソッド)を使ってAPI経由でデータを取得する
+  //       - https://developer.mozilla.org/ja/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+  //     2. fetchメソッドで取得したResponseデータからJSON形式のデータをオブジェクトに変換する
+  //       - https://developer.mozilla.org/ja/docs/Web/API/Response
+  //     3. 2で取得したデータの中にresultsプロパティ(配列)があり、その中には10件のクイズデータがある。
+  //        その10件のデータを利用してクイズの出題・回答を出来るようにする。(詳しくは完成形の画像を参照)
+  // - 引数
+  //   - 無し
+  // - 戻り値
+  //   - 無し
+  const fetchQuizData = async () => {
+    questionElement.textContent = 'Now loading...';
+    resultElement.textContent = '';
+    restartButton.hidden = true;
+
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      gameState.quizzes = data.results;
+      gameState.currentIndex = 0;
+      gameState.numberOfCorrects = 0;
+      setNextQuiz();
+    } catch (error) {
+      alert(`読み込み失敗... (${error.message})`);
+    }
+  };
+
+  // setNextQuiz関数を実装する
+  // - 実現したいこと
+  //   - 表示要素をリセットする
+  //     - 問題文を空にする
+  //     - 解答一覧を全て消す
+  //   - 条件に応じて、次の問題の表示 or 結果を表示する
+  //     - 次の問題がまだ存在すれば次の問題をセットする
+  //     - 直前に解答したのが最後の問題であれば結果を表示する(finishQuiz関数を実行する)
+  // - 引数
+  //   - 無し
+  // - 戻り値
+  //   - 無し
+  const setNextQuiz = () => {
+    // 問題文と解答を空にしてから、次の問題 or 結果を表示
+    questionElement.textContent = '';
+    removeAllAnswers();
+
+    if (gameState.currentIndex < gameState.quizzes.length ) {
+      const quiz = gameState.quizzes[gameState.currentIndex];
+      makeQuiz(quiz);
+    } else {
+      finishQuiz();
+    }
+  };
+
+  // finishQuiz関数を実装する
+  // - 実現したいこと
+  //   - 正答数を表示する(例: 4問正解したばあいは 「4/10 corrects」と表示する)
+  //   - 「Restart」ボタンを表示する
+  // - 引数
+  //   - 無し
+  // - 戻り値
+  //   - 無し
+  const finishQuiz = () => {
+    resultElement.textContent = `${gameState.numberOfCorrects}/${gameState.quizzes.length} corrects.`;
+    restartButton.hidden = false;
+  };
+
+  // removeAllAnswers関数を実装する
+  // - 実現したいこと
+  //   - 解答を全て削除する
+  // - 引数
+  //   - 無し
+  // - 戻り値
+  //   - 無し
+  const removeAllAnswers = () => {
+    while (answersContainer.firstChild) {
+      answersContainer.removeChild( answersContainer.firstChild );
+    }
+  };
+
+  // makeQuiz関数を実装する
+  // - 実現したいこと
+  //   - クイズデータを元にWebページ上に問題と解答リストを表示する
+  //   - 解答をクリックしたら、正解・不正解のチェックをする
+  //     - 正解の場合
+  //       - 「gameState」オブジェクトで管理している正答数プロパティをインクリメントする
+  //       - 「Correct answer!!」とアラート表示する
+  //     - 不正解の場合
+  //       - 「Wrong answer... (The correct answer is "実際の答えを埋め込む")」とアラート表示する
+  //   - 回答する度に「gameState」オブジェクトで管理している、問題数プロパティをインクリメントする
+  //   - setNextQuiz関数を実行して次の問題をセットする(最後の問題の場合は結果を表示する。)
+  // - 引数
+  //   - 無し
+  // - 戻り値無し
+  //   - 無し
+  const makeQuiz = (quiz) => {
+    // シャッフル済みの解答一覧を取得する
+    const answers = buildAnswers(quiz);
+
+    // 問題文のセット
+    questionElement.textContent = unescapeHTML(quiz.question);
+
+    // 解答一覧をセット
+    answers.forEach((answer) => {
+      const liElement = document.createElement('li');
+      liElement.textContent = unescapeHTML(answer);
+      answersContainer.appendChild(liElement);
+
+      // 解答を選択したときの処理
+      liElement.addEventListener('click', (event) => {
+        unescapedCorrectAnswer = unescapeHTML(quiz.correct_answer);
+        if (event.target.textContent === unescapedCorrectAnswer) {
+          gameState.numberOfCorrects++;
+          alert('Correct answer!!');
+        } else {
+          alert(`Wrong answer... (The correct answer is "${unescapedCorrectAnswer}")`);
+        }
+
+        gameState.currentIndex++;
+        setNextQuiz();
+      });
+    });
+  };
+
+  // quizオブジェクトの中にあるcorrect_answer, incorrect_answersを結合して
+  // 正解・不正解の解答をシャッフルする。
+  const buildAnswers = (quiz) => {
+    const answers = [
+      quiz.correct_answer,
+      ...quiz.incorrect_answers
+    ];
+
+    const shuffledAnswers = shuffle(answers);
+
+    return shuffledAnswers;
+  };
+
+  // `shuffle関数` を実装する
+  // - 実現したいこと
+  //   - 引数で受け取った配列内の値をシャッフルする
+  //   - 引数で渡された配列を上書きしないように、シャッフルする配列はコピーしたものを使う
+  //   - シャッフルの機能を実現するのに参考になるサイト
+  //     - https://qiita.com/artistan/items/9eb9a0fb14f4ec3a8764
+  // - 引数
+  //   - array : 配列
+  // - 戻り値
+  //   - shffuledArray : シャッフル後の配列(引数の配列とは別の配列であることに注意する)
+  const shuffle = (array) => {
+    const copiedArray = array.slice();
+    for (let i = copiedArray.length - 1; i >= 0; i--){
+      const rand = Math.floor( Math.random() * ( i + 1 ) );
+      [copiedArray[i], copiedArray[rand]] = [copiedArray[rand], copiedArray[i]]
+    }
+
+    return copiedArray;
+  };
+
+
+  // unescapeHTML関数を実装する
+  // - 実現したいこと
+  //   - &やクオーテーションマークなどが特殊文字としてセットされているので、
+  //     人間が読みやすい形式に変換した文字列を取得する(エスケープ文字を元に戻す)
+  //     参考にしたサイト : http://blog.tojiru.net/article/211339637.html
+  // - 引数
+  //   - 文字列
+  // - 戻り値
+  //   - 文字列
+  const unescapeHTML = (str) => {
+    var div = document.createElement("div");
+    div.innerHTML = str.replace(/</g,"&lt;")
+                       .replace(/>/g,"&gt;")
+                       .replace(/ /g, "&nbsp;")
+                       .replace(/\r/g, "&#13;")
+                       .replace(/\n/g, "&#10;");
+
+    return div.textContent || div.innerText;
+  };
+})();
